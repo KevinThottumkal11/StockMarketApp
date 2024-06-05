eadb851b06e8273107f4735549c37774326be474
Kevin Binu Thottumkal <kevin-binu-thottumkal@mohawkcollege.ca>
2024-06-05 01:35:12 -0400

    Added functions to the repository in the domain layer

--- app/src/main/java/com/plcoding/stockmarketapp/domain/repository/StockRepository.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/domain/repository/StockRepository.kt
@@ -1,6 +1,8 @@
 package com.plcoding.stockmarketapp.domain.repository
 
+import com.plcoding.stockmarketapp.domain.model.CompanyInfo
 import com.plcoding.stockmarketapp.domain.model.CompanyListing
+import com.plcoding.stockmarketapp.domain.model.IntradayInfo
 import com.plcoding.stockmarketapp.util.Resource
 import kotlinx.coroutines.flow.Flow
 
@@ -10,5 +12,13 @@ interface StockRepository {
         fetchFromRemote: Boolean,
         query: String
     ): Flow<Resource<List<CompanyListing>>> // domain layer should not access the data layer. Therefore, do not use CompanyListingEntity
+
+    suspend fun getIntrdayInfo(
+        symbol: String
+    ): Resource<List<IntradayInfo>>
+
+    suspend fun getCompnayInfo(
+        symbol: String
+    ): Resource<CompanyInfo>
 
 }
