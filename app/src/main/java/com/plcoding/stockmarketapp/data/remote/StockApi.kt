2041e6b59fe3c3aca4f3d7610dbb83a178af4dc3
Kevin Binu Thottumkal <kevin-binu-thottumkal@mohawkcollege.ca>
2024-06-05 01:25:05 -0400

    Added function to get Intrday Info into the Stock Api

--- app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/data/remote/StockApi.kt
@@ -11,6 +11,12 @@ interface StockApi {
         @Query("apikey") apiKey: String = API_KEY
     ): ResponseBody
 
+    @GET("query?function=TIME_SERIES_INTRADAY&interval=60min&datatype=csv")
+    suspend fun getIntradayInfo(
+        @Query("symbol") symbol: String,
+        @Query("apikey") apikey: String = API_KEY
+    ): ResponseBody
+
     companion object {
         const val API_KEY = "FTGFATU65KMFZDAJ"
         const val BASE_URL = "https://alphavantage.co"
