891c3a8c78fb8b217d909193dc62c443c494b078
Kevin Binu Thottumkal <kevin-binu-thottumkal@mohawkcollege.ca>
2024-07-12 01:23:04 -0400

    added state variables for the stock chart

---
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_info/StockChart.kt
@@ -0,0 +1,40 @@
+package com.plcoding.stockmarketapp.presentation.company_info
+
+import android.graphics.Paint
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.sp
+import com.plcoding.stockmarketapp.domain.model.IntradayInfo
+import kotlin.math.roundToInt
+
+@Composable
+fun StockChart(
+    infos: List<IntradayInfo> = emptyList(),
+    modifier: Modifier = Modifier,
+    graphColor: Color = Color.Green
+) {
+    val spacing = 100f
+    val transparentGraphColor = remember {
+        graphColor.copy(alpha = 0.5f)
+    }
+    val upperValue = remember(infos) {
+        (infos.maxOfOrNull { it.close }?.plus(1))?.roundToInt() ?: 0
+    }
+
+    val lowerValue = remember(infos) {
+        infos.minOfOrNull { it.close }?.toInt() ?: 0
+    }
+
+    val density = LocalDensity.current
+
+    val textPaint = remember {
+        Paint().apply {
+            color = android.graphics.Color.WHITE
+            textAlign = Paint.Align.CENTER
+            textSize = density.run { 12.sp.toPx() }
+        }
+    }
+}
