9682d7300b2ca896f26454b11dd26576bdf3f005
Kevin Binu Thottumkal <kevin-binu-thottumkal@mohawkcollege.ca>
2024-07-25 01:29:26 -0400

    implemented x-axis and y-axis for the graph

--- app/src/main/java/com/plcoding/stockmarketapp/presentation/company_info/StockChart.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_info/StockChart.kt
@@ -1,13 +1,16 @@
 package com.plcoding.stockmarketapp.presentation.company_info
 
 import android.graphics.Paint
+import androidx.compose.foundation.Canvas
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.sp
 import com.plcoding.stockmarketapp.domain.model.IntradayInfo
+import kotlin.math.round
 import kotlin.math.roundToInt
 
 @Composable
@@ -35,6 +38,36 @@ fun StockChart(
             color = android.graphics.Color.WHITE
             textAlign = Paint.Align.CENTER
             textSize = density.run { 12.sp.toPx() }
+        }
+    }
+    
+    Canvas(modifier = modifier) {
+        // x-axis of the graph
+        val spacePerHour = (size.width - spacing) / infos.size
+        (0 until infos.size step 2).forEach { i ->
+            val info = infos[i]
+            val hour = info.date.hour
+            drawContext.canvas.nativeCanvas.apply {
+                drawText(
+                    hour.toString(),
+                    spacing + i * spacePerHour,
+                    size.height - 5,
+                    textPaint
+                )
+            }
+        }
+
+        // y-axis of the graph
+        val priceStep = (upperValue - lowerValue) / 5f
+        (0..5).forEach{ i ->
+            drawContext.canvas.nativeCanvas.apply {
+                drawText(
+                    round(lowerValue + priceStep * i).toString(),
+                    30f,
+                    size.height - spacing - i * size.height / 5f,
+                    textPaint
+                )
+            }
         }
     }
 }
