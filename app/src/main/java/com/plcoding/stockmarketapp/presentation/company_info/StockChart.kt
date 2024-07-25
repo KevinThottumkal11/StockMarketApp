3ee255ff888fd7197401e26c5023516d581501c6
Kevin Binu Thottumkal <kevin-binu-thottumkal@mohawkcollege.ca>
2024-07-25 01:51:00 -0400

    completed implementing the graph

--- app/src/main/java/com/plcoding/stockmarketapp/presentation/company_info/StockChart.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_info/StockChart.kt
@@ -5,12 +5,16 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.asAndroidPath
 import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.plcoding.stockmarketapp.domain.model.IntradayInfo
 import kotlin.math.round
@@ -109,5 +113,27 @@ fun StockChart(
                 lineTo(spacing, size.height - spacing)
                 close()
             }
+
+        // graph gradient
+        drawPath(
+            path = fillPath,
+            brush = Brush.verticalGradient(
+                colors = listOf(
+                    transparentGraphColor,
+                    Color.Transparent
+                ),
+                endY = size.height - spacing
+            )
+        )
+
+        // graph line
+        drawPath(
+            path = strokePath,
+            color = graphColor,
+            style = Stroke(
+                width = 3.dp.toPx(),
+                cap = StrokeCap.Round
+            )
+        )
     }
 }
